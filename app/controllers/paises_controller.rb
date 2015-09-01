c2ada588a2f349404163683427ecf32591f92e23
blackruby <inaki60@gmail.com>
2015-09-01 13:02:19 +0200

    Estilos

--- app/controllers/paises_controller.rb
+++ app/controllers/paises_controller.rb
@@ -12,6 +12,10 @@ class PaisesMod < Pais
 
   #def ini_campos_ctrl
   #end
+
+  def vali_codigo_cr
+    return 'El código no puede tener dos caracteres' if codigo_cr.size == 2
+  end
 end
 
 class PaisesMod < Pais
