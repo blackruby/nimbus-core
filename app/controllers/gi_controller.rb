92e55dce458ff16e08f4239c5339eaa89d6875a8
blackruby <inaki60@gmail.com>
2018-09-07 19:26:21 +0200

    Mostrar campos ordenados en búsquedas y GI

--- app/controllers/gi_controller.rb
+++ app/controllers/gi_controller.rb
@@ -192,7 +192,7 @@ class GiController < ApplicationController
     emp = params[:emp].to_i
 
     data = []
-    cl.column_names.each {|c|
+    cl.column_names.sort.each {|c|
       d = {table: cl.table_name, type: cl.columns_hash[c].type}
       if c.ends_with?('_id')
         d[:id] = cl.reflect_on_association(c[0..-4].to_sym).options[:class_name]
