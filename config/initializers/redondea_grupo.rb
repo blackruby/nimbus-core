0f107d48e9bae48d0b645b4dee25cb5b07559ddc
blackruby <inaki60@gmail.com>
2018-10-25 13:59:21 +0200

    Nuevo método redondea_grupo

--- config/initializers/redondea_grupo.rb
+++ config/initializers/redondea_grupo.rb
@@ -1,24 +1,7 @@
+# Método para repartir proporcionalmente tot entre todos los números
+# del array nums redondeados a "decimales". Devuelve un array con el resultado.
+
 def redondea_grupo(tot:, nums:, decimales:)
-  return nums if (tot.class != BigDecimal && tot.class != Float) || nums.class != Array || decimales.class != Fixnum
-
-  redondeado = nums.map{|n| n.round(decimales)}
-  diferencia = redondeado.reduce(tot){|t,n| t - n}.round(decimales)
-  if diferencia != 0
-    (diferencia > 0) ? signo = 1 : signo = -1
-    (decimales == 0) ? paso = 1 : paso = 1.0 / 10 ** decimales
-    diferencia *= signo
-    decis = nums.map.with_index{|n,i| [i,n.modulo(1)]}.to_h.sort_by{|c,v| -v}.to_h
-    decis.each{|c,v|
-      break if diferencia == 0
-      paso = diferencia if diferencia < paso
-      redondeado[c] += paso * signo
-      diferencia -= paso
-    }
-  end
-  redondeado
-end
-
-def redondea_g(tot:, nums:, decimales:)
   tot = tot.round(decimales).to_d
   suma = nums.reduce(:+)
   mindif = [99, 0]
