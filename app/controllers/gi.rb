2d7594870554eb3c584314497021c216e19fc840
blackruby <inaki60@gmail.com>
2020-12-15 15:48:53 +0100

    Usar el path del fichero en GI::run

--- app/controllers/gi.rb
+++ app/controllers/gi.rb
@@ -322,7 +322,8 @@ class GI
 
       #`libreoffice --headless --convert-to pdf --outdir /tmp #{fns}.xlsx`
       dirtmp = "#{fx[0..-6]}_lo_dir"
-      `libreoffice -env:UserInstallation=file://#{dirtmp} --headless --convert-to pdf --outdir /tmp #{fx}`
+      #`libreoffice -env:UserInstallation=file://#{dirtmp} --headless --convert-to pdf --outdir /tmp #{fx}`
+      `libreoffice -env:UserInstallation=file://#{dirtmp} --headless --convert-to pdf --outdir #{Pathname(fx).dirname} #{fx}`
       FileUtils.rm_rf [fx, dirtmp]
       return fx[0..-5] + 'pdf'
     else
