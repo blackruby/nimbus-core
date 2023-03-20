52e5421b10b86ec51da2455a056e41b0a34871b8
blackruby <inaki60@gmail.com>
2023-03-20 19:16:38 +0100

    Adaptación a Rails 7.0.4

--- script/r7_tree/config/application.rb
+++ script/r7_tree/config/application.rb
@@ -5,6 +5,7 @@ Bundler.require(*Rails.groups)
 module Nimbus
   class Application < Rails::Application
     config.load_defaults 7.0
+    config.active_record.use_yaml_unsafe_load = true
   end
 end
 
