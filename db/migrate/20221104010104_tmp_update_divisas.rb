8b27307c0caa03b35ed898afbf96e1ece93d26ca
Iván Eguiluz <ieguiluz@terabyte2003.com>
2023-06-02 13:38:26 +0200

    Fix migrate tag 'excluir_divisas'

--- db/migrate/20221104010104_tmp_update_divisas.rb
+++ db/migrate/20221104010104_tmp_update_divisas.rb
@@ -1,13 +1,15 @@
 class TmpUpdateDivisas < ActiveRecord::Migration[5.0]
-  def change
-    unless Divisa.column_names.include?('prefijo')
-      add_column :divisas, :prefijo, :string
-      add_column :h_divisas, :prefijo, :string
-    end
+  unless Nimbus::Config[:excluir_divisas]
+    def change
+      unless Divisa.column_names.include?('prefijo')
+        add_column :divisas, :prefijo, :string
+        add_column :h_divisas, :prefijo, :string
+      end
 
-    unless Divisa.column_names.include?('sufijo')
-      add_column :divisas, :sufijo, :string
-      add_column :h_divisas, :sufijo, :string
+      unless Divisa.column_names.include?('sufijo')
+        add_column :divisas, :sufijo, :string
+        add_column :h_divisas, :sufijo, :string
+      end
     end
   end
 end
